2725e2ad0805fcf53368a5a0353841fbc04019fd
Pederzini <victor.pederzini@bandtec.com.br>
2021-02-10 11:02:33 -0300

    Início do desafio

---
+++ DesafioNumeros/src/Desafio/Main.java
@@ -0,0 +1,186 @@
+package Desafio;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    ArrayList<Integer> listaAleatorios = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.entradaNome();
+        main.entradaIdade();
+        System.out.println("Vamos jogar! Este programa sorteará 20 números aleatórios entre" +
+                "1 e 50. Você decidirá o que fazer com eles no menu abaixo.");
+        System.out.println("-------------------------------------------");
+        System.out.println("Opções disponíveis:");
+        System.out.println("1) Adivinhar se existe um número na lista");
+        System.out.println("2) Somar os 10 primeiros");
+        System.out.println("3) Subtrair os 10 primeiros");
+        System.out.println("4) Adivinhar o 1° número");
+        System.out.println("5) Multiplicar os pares");
+        System.out.println("6) Multiplicar os ímpares");
+        System.out.println("7) Ordenar lista -> menor para o maior");
+        System.out.println("8) Embaralhar lista -> randomizar");
+        System.out.println("9) Criar tabuada com os 5 primeiros números");
+        System.out.println("10) Somar todos os números");
+        System.out.println("0) Sair");
+        System.out.println("-------------------------------------------");
+        System.out.println("Digite o número da opção desejada:");
+        System.out.println("lalala");
+        Scanner inputEscolha = new Scanner(System.in);
+        int escolha = inputEscolha.nextInt();
+
+        switch (escolha) {
+            case 1 : main.adivinharNumero();
+                break;
+            case 2 : main.somar10();
+                break;
+            case 3 : main.subtrair10();
+                break;
+            case 4 : main.adivinharPrimeiro();
+                break;
+            case 5 : main.multiplicarPares();
+                break;
+            case 6 : main.multiplicarImpares();
+                break;
+            case 7 : main.ordenarListaAsc();
+                break;
+            case 8 : main.embaralharLista();
+                break;
+            case 9 : main.tabuada5Primeiros();
+                break;
+            case 10 : main.somarTodos();
+                break;
+            case 0 : main.sair();
+                break;
+        }
+    }
+
+    public static int numAleatorio() {
+        int aleatorio = (int) ((Math.random()*51)+1);
+        return aleatorio;
+    }
+
+    public ArrayList<Integer> preencherLista() {
+
+        for(int i=0; i<20; i++){
+            numAleatorio();
+            listaAleatorios.add(numAleatorio());
+        }
+        for(int i=0; i<20; i++){
+            if(i==0) {
+                System.out.print("[" + listaAleatorios.get(i) + ", ");
+            } else if (i<19) {
+                System.out.print(listaAleatorios.get(i) + ", ");
+            } else {
+                System.out.println(listaAleatorios.get(i) + "]");
+            }
+        }
+        return listaAleatorios;
+    }
+
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
+
+    public String entradaNome() {
+        boolean errado = true;
+        String nome = "";
+        while(errado) {
+            System.out.println("Digite seu nome:");
+            Scanner inputNome = new Scanner(System.in);
+            nome = inputNome.nextLine();
+            if(isNumeric(nome)) {
+                System.out.println("Por favor, não utilize números!");
+            } else {
+                errado = false;
+            }
+        }
+        return nome;
+    }
+
+    public int entradaIdade() {
+        boolean errado = true;
+        int idade = 0;
+        while(errado) {
+            try {
+                System.out.println("Digite sua idade:");
+                Scanner inputIdade = new Scanner(System.in);
+                idade = inputIdade.nextInt();
+                errado = false;
+            } catch (Exception e) {
+                System.out.println("Por favor, não utilize letras!");
+                errado = true;
+            }
+        }
+        return idade;
+    }
+
+    public boolean adivinharNumero() {
+        System.out.println("Em produção");
+        return false;
+    }
+
+    public int somar10() {
+        System.out.println("Em produção");
+        return 0;
+    }
+
+    public int subtrair10() {
+        System.out.println("Em produção");
+        return 0;
+    }
+
+    public boolean adivinharPrimeiro() {
+        System.out.println("Em produção");
+        return false;
+    }
+
+    public int multiplicarPares() {
+        System.out.println("Em produção");
+        return 0;
+    }
+
+    public int multiplicarImpares() {
+        System.out.println("Em produção");
+        return 0;
+    }
+
+    public ArrayList<Integer> ordenarListaAsc() {
+        System.out.println("Em produção");
+        return preencherLista();
+    }
+
+    public ArrayList<Integer> embaralharLista() {
+        System.out.println("Em produção");
+        return preencherLista();
+    }
+
+    public String tabuada5Primeiros() {
+        System.out.println("Em produção");
+        return "";
+    }
+
+    public void somarTodos() {
+        int soma = 0;
+        // for-each, diferente do for tradicional
+        // usado quando quero usar TODOS os itens da lista
+        for(int i : preencherLista()){
+            soma += i;
+        }
+        System.out.println("A soma de todos os números é: " + soma);
+    }
+
+    public void sair() {
+        System.out.println("Obrigado por jogar!");
+    }
+
+}
