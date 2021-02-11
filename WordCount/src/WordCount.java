d97fd0be85a73c808f811c289dbf1d39b3c0a877
Pederzini <victor.pederzini@bandtec.com.br>
2021-02-11 14:06:04 -0300

    Finalizar projeto WordCount

--- WordCount/src/WordCount.java
+++ WordCount/src/WordCount.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+import java.io.File;
+
 public class WordCount {
+    public static void main(String[] args) throws Exception{
+        File file = new File("pg50133.txt");
+        Scanner scanner = new Scanner(file);
+
+        Integer words = 0;
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            // To count the number of words in that line
+            // we'll want to split it on the spaces
+            words += line.split(" ").length;
+        }
+
+        System.out.println("The file contains: " + words + " words.");
+    }
 }
